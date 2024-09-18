6be724b1e19795f260e6ee2d91f58b8d8def74c5
alzinan1234 <pappurohan1100@gmail.com>
2024-09-18 18:58:04 +0600

    banner

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,5 +1,21 @@
+import image from "../../assets/image_here.png";
+import bgImage from "../../assets/background.png";
+
 const Banner = () => {
-  return <></>;
+  return (
+    <>
+      <div
+        className="w-[1075px] h-auto bg-cover bg-no-repeat bg-center"
+        style={{
+          backgroundImage: `url(${bgImage})`,
+        }}
+      >
+        <div className="flex justify-center items-center">
+          <img className="w-[845px] h-[880px]" src={image} alt="Banner Image" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Banner;
--- src/components/page/Home/Home.jsx
+++ src/components/page/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../home/Navbar";
+import Banner from "../../home/Banner";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Banner />
     </>
   );
 };
